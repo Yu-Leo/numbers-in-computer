98b09ed04f158d87ef7144c8a77ea1242c256d62
Lev <66430048+Yu-Leo@users.noreply.github.com>
2021-01-07 16:34:28 +0300

    Add clear_button mechanics
    Добавлена механика работы кнопки очиски полей ввода.
    Изменён размер поля для ввода числа двоичных разрядов

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from WindowParametes import WindowParameters
 from Widgets import *
 
 
+def calculate():
+    pass
+
+
 class Window:
     def __init__(self):
         self.__root = tk.Tk()
@@ -18,7 +22,12 @@ class Window:
         self.labels = LabelsList(self.__root)
         self.radiobuttons = RadiobuttonsList(self.__root)
         self.entries = EntriesList(self.__root)
-        self.buttons = ButtonsList(self.__root)
+        self.buttons = ButtonsList(self.__root, self.clear_entries, calculate)
+
+    def clear_entries(self):
+        """Очищает все поля кроме задающих настройки"""
+        for i in range(1, len(self.entries.list)):
+            self.entries.list[i].delete(0, tk.END)
 
     def draw_widgets(self):
         self.radiobuttons.list[0].grid(row=0, column=0)
@@ -29,12 +38,15 @@ class Window:
                                      pady=10)
         for i in range(5):
             self.radiobuttons.list[2 + i].grid(row=(2 + i), column=1)
-        for i in range(6):
+
+        self.entries.list[0].grid(row=1, column=2, sticky=tk.W, padx=20)
+
+        for i in range(1, 6):
             self.entries.list[i].grid(row=i + 1, column=2)
 
-        self.buttons.list[0].grid(row=7, column=0, sticky=tk.W, padx=20,
-                                  pady=10)
-        self.buttons.list[1].grid(row=7, column=2, padx=20, pady=10)
+        self.buttons.clear_button.grid(row=7, column=0, sticky=tk.W, padx=20,
+                                       pady=10)
+        self.buttons.calculate_button.grid(row=7, column=2, padx=20, pady=10)
 
     def run(self):
         self.draw_widgets()

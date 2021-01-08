f14cdb8ecbbf7885343d164b4af1f54a53db72b7
Lev <66430048+Yu-Leo@users.noreply.github.com>
2021-01-08 17:04:53 +0300

    Update mechanics v.0.3
    1) Набор параметров для целочисленного перевода вынесен в класс и в отдельный файл
    2) Переменные и классы, задействованные при целочисленном переводе, переименованы с приставкой int-
    3) Константа максимального числа двоичных разрядов вынесена в файл с константами

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import tkinter as tk
 
 from messageboxes import Messageboxes as Mb
 from windowParametes import WindowParameters
-from widgets import Widgets
-import mechanics
+from widgets import IntWidgets
+import intMechanics
 import constants as c
 import exceptions as e
 
@@ -20,38 +20,40 @@ class Window:
         self.__root.geometry(self.__parameters.get_geometry())
         self.__root.resizable(*self.__parameters.get_resizable())
         self.__root.iconbitmap(self.__parameters.get_ico_path())
-        self.widgets = Widgets(self.__root, self.calculate_result, self.copy_to_buffer)
+        self.int_widgets = IntWidgets(self.__root, self.int_calculate, self.copy_to_buffer)
 
-    def calculate_result(self):
+    def int_calculate(self):
         try:
-            mechanics.calculate(self.widgets.int_entries)
+            intMechanics.calculate(self.int_widgets.int_entries)
         except e.BinSizeTypeError:
             Mb.BinSizeTypeError.show()
-            self.widgets.int_entries.clear_all_except()
+            self.int_widgets.int_entries.clear_all_except()
         except e.BinSizeValueError:
             Mb.BinSizeValueError.show()
-            self.widgets.int_entries.clear_all_except(c.Int.BIN_SIZE_INDEX)
+            self.int_widgets.int_entries.clear_all_except(c.Int.BIN_SIZE_INDEX)
 
         except e.DecNumTypeError:
             Mb.DecNumTypeError.show()
-            self.widgets.int_entries.clear_all_except(c.Int.BIN_SIZE_INDEX)
+            self.int_widgets.int_entries.clear_all_except(c.Int.BIN_SIZE_INDEX)
         except e.DecNumValueCodesWarning:
             Mb.DecNumValueCodesWarning.show()
-            self.widgets.int_entries.clear_all_except(c.Int.BIN_SIZE_INDEX, c.Int.DEC_NUM_INDEX, c.Int.BIN_NUM_INDEX)
+            self.int_widgets.int_entries.clear_all_except(c.Int.BIN_SIZE_INDEX, c.Int.DEC_NUM_INDEX,
+                                                          c.Int.BIN_NUM_INDEX)
 
         except e.BinNumTypeError:
             Mb.BinNumTypeError.show()
-            self.widgets.int_entries.clear_all_except(c.Int.BIN_SIZE_INDEX)
+            self.int_widgets.int_entries.clear_all_except(c.Int.BIN_SIZE_INDEX)
         except e.BinNumValueCodesWarning:
             Mb.BinNumValueCodesWarning.show()
-            self.widgets.int_entries.clear_all_except(c.Int.BIN_SIZE_INDEX, c.Int.DEC_NUM_INDEX, c.Int.BIN_NUM_INDEX)
+            self.int_widgets.int_entries.clear_all_except(c.Int.BIN_SIZE_INDEX, c.Int.DEC_NUM_INDEX,
+                                                          c.Int.BIN_NUM_INDEX)
 
     def copy_to_buffer(self, index):
-        mechanics.copy_val_to_buffer(self.widgets.int_entries, index)
+        intMechanics.copy_val_to_buffer(self.int_widgets.int_entries, index)
 
     def run(self):
         """Запуск приложения"""
-        self.widgets.draw_int()
+        self.int_widgets.draw_int()
         self.__root.mainloop()
 
 

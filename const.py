5722c7ec2ea78c672c14f1756adb91f8695361bb
Lev Yuvenskiy <levayu22@gmail.com>
2021-01-08 12:31:47 +0300

    Add copy_buttons
    1) Добавлены кнопки справа от полей ввода-вывода, при нажатии на которые значение из соотв. поля копируется в буффер обмена Windows
    2) Добавлены константы с индексами строк

---
+++ const.py
@@ -0,0 +1,9 @@
+# Файл с константами для приложения
+
+class Int:
+    BIN_SIZE_INDEX = 0
+    DEC_NUM_INDEX = 1
+    BIN_NUM_INDEX = 2
+    STRAIGHT_CODE_INDEX = 3
+    REVERSED_CODE_INDEX = 4
+    ADDITIONAL_CODE_INDEX = 5

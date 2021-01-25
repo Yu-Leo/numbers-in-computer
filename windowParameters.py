29baecc44135b5b9c15c26987cb5fbeb5716c1c1
Lev <66430048+Yu-Leo@users.noreply.github.com>
2021-01-25 19:48:14 +0300

    add try-except for icons
    Добавлена обработка ошибок открытия файлов иконок (для кнопок и иконки приложения)

--- windowParameters.py
+++ windowParameters.py
@@ -8,7 +8,7 @@ class WindowParameters:
         self.__padx = 100  # Отступ по горизонтали от верхнего левого угра экрана
         self.__pady = 100  # Отступ по вертикали от верхнего левого угра экрана
         self.resizable = (False, False)
-        self.ico_path = "img/program_icon64.ico"
+        self.ico_path = "img/program_icon64.ico"  # Путь к иконке приложения
 
     def geometry(self):
         """Геометрия окна по стандарту tkinter-a"""

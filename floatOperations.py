1551f2880bf3a756661c31de090fbc2241346dbd
Lev Yuvenskiy <levayu22@gmail.com>
2021-02-02 19:33:06 +0300

    add actions for copy-buttons v0.1
    При нажатии на кнопку "рассчитать" ф-ция расчёта получает значения параметров

--- floatOperations.py
+++ floatOperations.py
@@ -2,6 +2,7 @@
 
 import pyperclip  # Модуль для работы с буфером
 
+import config
 import constants as c
 
 
@@ -14,7 +15,34 @@ def calculate(entries):
 
 
 def float_calc(entries):
-    pass
+    mantissa_bin_size = get_mantissa_bin_size(entries)
+    order_bin_size = get_order_bin_size(entries)
+    save_first_digit = get_save_first_digit(entries)
+    # Очистка от старых значений
+    entries.clear_all_except(c.Float.MANTISSA_BIN_SIZE_INDEX,
+                             c.Float.ORDER_BIN_SIZE_INDEX, config.translate_type)
+
+
+def get_mantissa_bin_size(entries):
+    str_mantissa_bin_size = entries.get_mantissa_bin_size()
+    try:
+        int_mantissa_bin_size = int(str_mantissa_bin_size)
+    except ValueError:
+        raise ValueError
+    return int_mantissa_bin_size
+
+
+def get_order_bin_size(entries):
+    str_order_bin_size = entries.get_order_bin_size()
+    try:
+        int_order_bin_size = int(str_order_bin_size)
+    except ValueError:
+        raise ValueError
+    return int_order_bin_size
+
+
+def get_save_first_digit(entries):
+    return entries.get_save_first_digit()
 
 
 def copy_to_buffer(entries, index):

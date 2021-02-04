ca3a332f7b4ddec347e146fc2f3ab5f402dbd398
Lev <66430048+Yu-Leo@users.noreply.github.com>
2021-02-04 21:10:15 +0300

    refactor exceptions
    Скопированные вручную изменения из ветки refactor-exceptions

--- messageboxes.py
+++ messageboxes.py
@@ -1,10 +1,12 @@
 # Файл с классами messagebox-ов
 
 import tkinter.messagebox as mb
-from text import Exceptions
+
+from constants import Exceptions as ConstEx
+from text import int_exceptions as intEx
 
 
-class Warning:
+class WarningMb:
     """Messagebox типа Warning"""
 
     def __init__(self, title, message):
@@ -15,7 +17,7 @@ class Warning:
         mb.showwarning(self.title, self.message)
 
 
-class Error:
+class ErrorMb:
     """Messagebox типа Error"""
 
     def __init__(self, title, message):
@@ -26,40 +28,17 @@ class Error:
         mb.showerror(self.title, self.message)
 
 
-class Messageboxes:
+class ExceptionMb(ErrorMb, WarningMb):
     """Messagebox-ы самописных ошибок"""
-    BinSizeTypeError = Error(title=Exceptions.bin_size.title,
-                             message=Exceptions.bin_size.type_error)
 
-    BinSizeValueError = Error(title=Exceptions.bin_size.title,
-                              message=Exceptions.bin_size.value_error)
-
-    DecNumTypeError = Error(title=Exceptions.dec_num.title,
-                            message=Exceptions.dec_num.type_error)
-
-    DecNumValueCodesWarning = Warning(title=Exceptions.dec_num.title,
-                                      message=Exceptions.dec_num.value_error)
-
-    BinNumTypeError = Error(title=Exceptions.bin_num.title,
-                            message=Exceptions.bin_num.type_error)
-
-    BinNumValueCodesWarning = Warning(title=Exceptions.bin_num.title,
-                                      message=Exceptions.bin_num.value_error)
-
-    StrCodeTypeError = Error(title=Exceptions.str_code.title,
-                             message=Exceptions.str_code.type_error)
-
-    StrCodeValueError = Error(title=Exceptions.str_code.title,
-                              message=Exceptions.str_code.value_error)
-
-    RevCodeTypeError = Error(title=Exceptions.rev_code.title,
-                             message=Exceptions.rev_code.type_error)
-
-    RevCodeValueError = Error(title=Exceptions.rev_code.title,
-                              message=Exceptions.rev_code.value_error)
-
-    AddCodeTypeError = Error(title=Exceptions.add_code.title,
-                             message=Exceptions.add_code.type_error)
-
-    AddCodeValueError = Error(title=Exceptions.add_code.title,
-                              message=Exceptions.add_code.value_error)
+    def __init__(self, exception):
+        if exception.type == ConstEx.TYPE_ERROR:
+            ErrorMb.__init__(self, title=intEx[exception.field].title,
+                             message=intEx[exception.field].type_error)
+        elif exception.type == ConstEx.RANGE_ERROR:
+            ErrorMb.__init__(self, title=intEx[exception.field].title,
+                             message=intEx[exception.field].range_error)
+        elif exception.type == ConstEx.WARNING:
+            WarningMb.__init__(self, title="", message="")
+        else:
+            raise Warning("Exception type error")

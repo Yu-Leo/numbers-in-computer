b14ef9c8fce6e194c421a8dc2ce02824304c9c93
Lev <Levayu22@gmail.com>
2021-05-28 13:37:16 +0300

    add unit-tests for int-type from dec

---
+++ testNumbersKits.py
@@ -0,0 +1,39 @@
+import unittest
+
+from numbersKits import IntKit, FloatKit
+
+class TestIntKit(unittest.TestCase):
+    """Тестируем работу с целыми числами"""
+
+    def setUp(self):
+        self.kit = None
+
+    def test_from_dec_1(self):
+        """Тестируем перевод из 10й с. с. в остальные"""
+        self.kit = IntKit(5)
+        self.kit.by_dec_num(4)
+        self.assertEqual(self.kit["dec_num"], 5)
+        self.assertEqual(self.kit["bin_num"], "101")
+        self.assertEqual(self.kit["str_code"], "0101")
+        self.assertEqual(self.kit["rev_code"], "0101")
+        self.assertEqual(self.kit["add_code"], "0101")
+
+    def test_from_dec_2(self):
+        """Тестируем перевод из 10й с. с. в остальные"""
+        self.kit = IntKit(0)
+        self.kit.by_dec_num(8)
+        self.assertEqual(self.kit["dec_num"], 0)
+        self.assertEqual(self.kit["bin_num"], "0")
+        self.assertEqual(self.kit["str_code"], "00000000")
+        self.assertEqual(self.kit["rev_code"], "00000000")
+        self.assertEqual(self.kit["add_code"], "00000000")
+
+    def test_from_dec_3(self):
+        """Тестируем перевод из 10й с. с. в остальные"""
+        self.kit = IntKit(-5)
+        self.kit.by_dec_num(8)
+        self.assertEqual(self.kit["dec_num"], -5)
+        self.assertEqual(self.kit["bin_num"], "-")
+        self.assertEqual(self.kit["str_code"], "10000101")
+        self.assertEqual(self.kit["rev_code"], "11111010")
+        self.assertEqual(self.kit["add_code"], "11111011")

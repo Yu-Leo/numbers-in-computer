0d98efc3411b141213e7719bcc9f279bc8bd1887
Lev Yuvensky <levayu22@gmail.com>
2021-11-28 21:43:45 +0300

    Replace add 'buffer' with 'clipboard'

--- floatOperations.py
+++ floatOperations.py
@@ -167,7 +167,7 @@ def get_float_format(entries, mantissa, exponent, save):
     return float_format
 
 
-def copy_to_buffer(entries, index):
+def copy_to_clipboard(entries, index):
     """
     Copy value from entry to clipboard by its index
     :param entries: list of tkinter's entries objects
